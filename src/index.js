43584b244ebe2c192f74f18c350bbcc433933276
andr3arias <santiyabwana@gmail.com>
2020-02-26 15:32:26 -0500

    actualizacion

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+import '../src/global.css'
+import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge 
+    firstName='Andre' 
+    lastName='Arias' 
+    jobTitle="Frontend Engineer" 
+    twitter='andr3arias' 
+    avatarUrl="https://es.gravatar.com/userimage/180410375/bde1659c9a07c3da0976375913c2a815.jpg" 
+    />, container);
